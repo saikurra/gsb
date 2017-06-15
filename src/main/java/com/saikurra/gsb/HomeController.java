1da09305d19ecf11042478a2dcd40741811ac3c2
Sai Kurra <srinadh.a840@gmail.com>
2017-06-15 00:19:04 -0500

    changes dependencies and added tomcat plugin

--- src/main/java/com/saikurra/gsb/HomeController.java
+++ src/main/java/com/saikurra/gsb/HomeController.java
@@ -1,7 +1,5 @@
 package com.saikurra.gsb;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.saikurra.gsb.model.EngineeringDetails;
-import com.saikurra.gsb.model.Univ;
 import com.saikurra.gsb.model.UnivDetails;
 
 /**
@@ -17,41 +14,34 @@ import com.saikurra.gsb.model.UnivDetails;
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	
-	  @RequestMapping(value = "/", method = RequestMethod.GET)
-	  public ModelAndView home() {
-		
-			/*logger.info("Welcome home! The client locale is {}.", locale);*/
-			ModelAndView mv= new ModelAndView();
-			mv.addObject("univDetails", new UnivDetails());
-			mv.addObject("enggDetails", new EngineeringDetails());
-			mv.setViewName("adduniv");
-			return mv;
-	   }
-	
-	  @RequestMapping(value="/submitUnivDetails", method = RequestMethod.POST)
-	   public ModelAndView addStudent(@ModelAttribute("univDetails") UnivDetails univDetails) {
-		  logger.debug("University Details Submitted");
-		  ModelAndView mv= new ModelAndView();
-		  mv.addObject("univDetails", univDetails);
-		  mv.addObject("enggDetails", new EngineeringDetails());
-		  mv.setViewName("adduniv");
-	      return mv;
-	   }
-	  
 
-	  @RequestMapping(value="/submitEnggDetails", method = RequestMethod.POST)
-	   public ModelAndView addEnggDetails(@ModelAttribute("enggDetails") EngineeringDetails enggDetails) {
-		  logger.debug("Engg Details Submitted");
-		  ModelAndView mv= new ModelAndView();
-		  mv.addObject("univDetails", new UnivDetails());
-		  mv.addObject("enggDetails", enggDetails);
-		  mv.setViewName("adduniv");
-	  		return mv;
-	   }
-	  
-	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home() {
+
+		/* logger.info("Welcome home! The client locale is {}.", locale); */
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("univDetails", new UnivDetails());
+		mv.addObject("enggDetails", new EngineeringDetails());
+		mv.setViewName("adduniv");
+		return mv;
+	}
+
+	@RequestMapping(value = "/submitUnivDetails", method = RequestMethod.POST)
+	public ModelAndView addStudent(@ModelAttribute("univDetails") UnivDetails univDetails) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("univDetails", univDetails);
+		mv.addObject("enggDetails", new EngineeringDetails());
+		mv.setViewName("adduniv");
+		return mv;
+	}
+
+	@RequestMapping(value = "/submitEnggDetails", method = RequestMethod.POST)
+	public ModelAndView addEnggDetails(@ModelAttribute("enggDetails") EngineeringDetails enggDetails) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("univDetails", new UnivDetails());
+		mv.addObject("enggDetails", enggDetails);
+		mv.setViewName("adduniv");
+		return mv;
+	}
+
 }
